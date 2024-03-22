07bacad4a7c105da430365470c11e438d2b45994
Mpethwane3110 <mpethwanenomfndo@gmail.com>
2024-03-22 22:47:45 +0000

    lockboxes

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-# lockboxes
+"""Solution to Lockboxes problem"""
+
 
 def canUnlockAll(boxes):
-    x = len(boxes)
-    item = [False] * x
-    item[0] = True
-    stack = [0]
-
-    while stack:
-        box = stack.pop()
-        for each in boxes[box]:
-            if each >= 0 and each < x and not item[each]:
-                item[each] = True
-                stack.append(each)
-
-    return all(item)
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
